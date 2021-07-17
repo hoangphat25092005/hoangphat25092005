b556633ba1d97273adc017a64dcf7b344d04fdd1
hoangphat25092005 <87558213+hoangphat25092005@users.noreply.github.com>
2021-07-17 12:02:18 +0700

    Update hoangphat.py

--- hoangphat.py
+++ hoangphat.py
@@ -1,1 +1,7 @@
-print('hoangphat')
+n = int(input('please enter your passcode to started')
+if your passcode == 25092005 or passcode == 8122005:
+        print('welcome to github')
+else:
+        print('your passcode was wrong, please try again')
+        
+        
